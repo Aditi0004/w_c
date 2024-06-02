d32bc34580c2d71996b94034f4e88e0a15941107
Aditi0004 <139478822+Aditi0004@users.noreply.github.com>
2024-06-03 00:25:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 function getWordCount(str) {
-    let diff=str.trim().split(/\s+/);
-    console.log(diff);
-    if(diff==''){
-        return 0;
-    }
-    return diff.length;
+    let words = str.trim().split(/\s+/);
+    return str.trim() === '' ? 0 : words.length;
 }
-function counter(str){
-    var alphabets=0;
-    var numbers=0;
-    var totalenteries=0;
-    for(var i=0;i<str.length;i++){
-        if(str[i]!=' ' && str[i]!='\n'){
-            totalenteries++;
+
+function counter(str) {
+    let alphabets = 0;
+    let numbers = 0;
+    let totalEntries = 0;
+
+    for (let char of str) {
+        if (char.trim()) {
+            totalEntries++;
         }
-        if((str[i]>='a'&&str[i]<='z') || (str[i]>='A' && str[i]<='Z')){
+        if (/[a-zA-Z]/.test(char)) {
             alphabets++;
-        }
-        else if(str[i]>='0' && str[i]<='9'){
+        } else if (/\d/.test(char)) {
             numbers++;
         }
     }
-    document.getElementById("enteries").value = totalenteries;
+
+    document.getElementById("enteries").value = totalEntries;
     document.getElementById("numbers").value = numbers;
     document.getElementById("alphabets").value = alphabets;
 }
-function wordCounter()
-{
-    let inputTxt = document.getElementById("inputBox");    
-    let words = getWordCount(inputTxt.value);
+
+function wordCounter() {
+    let inputTxt = document.getElementById("inputBox").value;
+    let words = getWordCount(inputTxt);
     document.getElementById("words").value = words;
-    counter(inputTxt.value);
+    counter(inputTxt);
 }
